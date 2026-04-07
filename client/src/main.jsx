80c351ebdc32f77d3d87811653c71a99f21127a2
siddartha216 <siddarthayadavguntla@siddarthas-MacBook-Air.local>
2026-04-07 16:02:39 +0530

    final stable

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,21 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
-import { AppProvider } from './Context/AppContext.jsx'
+import { AppProvider } from './Context/AppContext'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env file')
 }
+
 createRoot(document.getElementById('root')).render(
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignoutUrl="/">
-       <BrowserRouter>
-        <AppProvider>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignoutUrl="/">
+    <BrowserRouter>
+      <AppProvider>
         <App />
-          </AppProvider>
-       </BrowserRouter>,
-     </ClerkProvider>
+      </AppProvider>
+    </BrowserRouter>
+  </ClerkProvider>
 )
